b13c778b8c1a7c47572ffecee0f015a5c7068d6a
Jemyishai <jesse.moskowitz@gmail.com>
2020-09-04 08:12:44 -0400

    1.10 unicafe step5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,12 +6,13 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const allState = [{ good: good }, { neutral: neutral }, { bad: bad }];
-
   const all = good + neutral + bad;
   const average = (good * 1 + neutral * 0 + bad * -1) / all;
   const pos = (100 * good) / all;
+
+  const allState = [{ good: good }, { neutral: neutral }, { bad: bad }];
+
+
 
   const clickEvent = (event) => {
     if (event === "g") {
@@ -49,10 +50,12 @@ const App = () => {
     return (
       <>
         <h1>give feedback</h1>
-        <Stats
+        <Statistics
           sectionHeader="statistics"
-          allState={allState}
           clickEvent={clickEvent}
+          good={good}
+          neutral={neutral}
+          bad={bad}
           average={average}
           all={all}
           pos={pos}
@@ -75,13 +78,10 @@ const Clicks = ({ allState }) => {
   );
 };
 
-const All = ({ allNum }) => <>all {allNum}</>;
+const Statistic = ({ text, stat , symbol }) => <p>{text}{" "}{stat}{" "}{symbol}</p>;
 
-const Average = ({ average }) => <div>average {average}</div>;
 
-const Pos = ({ pos }) => <div>pos {pos} %</div>;
-
-const Stats = ({ sectionHeader, allState, clickEvent, average, all, pos }) => {
+const Statistics = ({ sectionHeader, good, neutral, bad, clickEvent, average, all, pos }) => {
   return (
     <div>
       <h1>{sectionHeader}</h1>
@@ -101,13 +101,25 @@ const Stats = ({ sectionHeader, allState, clickEvent, average, all, pos }) => {
         clickEvent={clickEvent}
       />
 
-      <Clicks allState={allState} />
-      <Average average={average} />
-      <All allNum={all} />
-      <Pos pos={pos} />
+     {/* <Clicks allState={allState} /> */}
+      <Statistic text={'good'} stat={good} />
+      <Statistic text={'neutral'} stat={neutral} />
+      <Statistic text={'bad'} stat={bad} />
+      <Statistic text={'average'} stat={average} />
+      <Statistic text={'all'} stat={all} />
+      <Statistic text={'positive'} stat={pos} symbol={'%'}/>
     </div>
   );
 };
+
+{/*
+  //Need to clean up buttons, Statistics Component, Clicks component, allState storage
+
+  const Buttons = () => (
+  <Button handle
+)
+*/}
+
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
