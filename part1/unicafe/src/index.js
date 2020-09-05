627347cc693c388e3680bf6288f442a9f16238f2
Jemyishai <jesse.moskowitz@gmail.com>
2020-09-05 08:36:50 -0400

    1.10 unicafe step5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -12,8 +12,6 @@ const App = () => {
 
   const allState = [{ good: good }, { neutral: neutral }, { bad: bad }];
 
-
-
   const clickEvent = (event) => {
     if (event === "g") {
       setGood(good + 1);
@@ -23,27 +21,14 @@ const App = () => {
       setBad(bad + 1);
     }
   };
-  if (all === 0){
+
+  if (all === 0) {
     return (
       <>
         <h1>give feedback</h1>
-        <Button
-        handleClick={() => clickEvent("g")}
-        text={"good"}
-        clickEvent={clickEvent}
-      />
-      <Button
-        handleClick={() => clickEvent("n")}
-        text={"neutral"}
-        clickEvent={clickEvent}
-      />
-      <Button
-        handleClick={() => clickEvent("b")}
-        text={"bad"}
-        clickEvent={clickEvent}
-      />
-      <h1>statistics</h1>
-      <p>No feedback given</p>
+        <Buttons clickEvent={clickEvent} />
+        <h1>statistics</h1>
+        <p>No feedback given</p>
       </>
     );
   } else {
@@ -65,49 +50,34 @@ const App = () => {
   }
 };
 
-const Clicks = ({ allState }) => {
-  console.log(allState);
-  return (
-    <>
-      {allState.map((obj) => {
-        for (const aProp in obj) {
-          return <p key={aProp + 1}> {aProp + " " + obj[aProp]} </p>;
-        }
-      })}
-    </>
-  );
-};
+const Statistic = ({ text, stat, symbol }) => (
+  <p>
+    {text} {stat} {symbol}
+  </p>
+);
 
-const Statistic = ({ text, stat , symbol }) => <p>{text}{" "}{stat}{" "}{symbol}</p>;
-
-
-const Statistics = ({ sectionHeader, good, neutral, bad, clickEvent, average, all, pos }) => {
+const Statistics = ({
+  sectionHeader,
+  good,
+  neutral,
+  bad,
+  clickEvent,
+  average,
+  all,
+  pos,
+}) => {
   return (
     <div>
       <h1>{sectionHeader}</h1>
-      <Button
-        handleClick={() => clickEvent("g")}
-        text={"good"}
-        clickEvent={clickEvent}
-      />
-      <Button
-        handleClick={() => clickEvent("n")}
-        text={"neutral"}
-        clickEvent={clickEvent}
-      />
-      <Button
-        handleClick={() => clickEvent("b")}
-        text={"bad"}
-        clickEvent={clickEvent}
-      />
 
-     {/* <Clicks allState={allState} /> */}
-      <Statistic text={'good'} stat={good} />
-      <Statistic text={'neutral'} stat={neutral} />
-      <Statistic text={'bad'} stat={bad} />
-      <Statistic text={'average'} stat={average} />
-      <Statistic text={'all'} stat={all} />
-      <Statistic text={'positive'} stat={pos} symbol={'%'}/>
+      <Buttons clickEvent={clickEvent} />
+
+      <Statistic text={"good"} stat={good} />
+      <Statistic text={"neutral"} stat={neutral} />
+      <Statistic text={"bad"} stat={bad} />
+      <Statistic text={"average"} stat={average} />
+      <Statistic text={"all"} stat={all} />
+      <Statistic text={"positive"} stat={pos} symbol={"%"} />
     </div>
   );
 };
@@ -115,11 +85,23 @@ const Statistics = ({ sectionHeader, good, neutral, bad, clickEvent, average, al
 {/*
   //Need to clean up buttons, Statistics Component, Clicks component, allState storage
 
-  const Buttons = () => (
-  <Button handle
-)
+   {/*
+      <Button handleClick={() => clickEvent("g")} text={"good"} />
+      <Button handleClick={() => clickEvent("n")} text={"neutral"} />
+      <Button handleClick={() => clickEvent("b")} text={"bad"} />
+
+       <Clicks allState={allState} />
+
 */}
 
+const Buttons = ({ clickEvent }) => (
+  <div>
+  {
+    [['g','good'],['n','neutral'],['b','bad']].map((arr)=><Button handleClick={() => clickEvent(arr[0])} text={arr[1]} />
+  )
+  }
+  </div>
+);
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
