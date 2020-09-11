ac41cf65055b004db672f4ebecb24605f4b15d17
Jemyishai <jesse.moskowitz@gmail.com>
2020-09-11 08:17:23 -0400

    phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,32 +1,65 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const submitName = (event) => {
     event.preventDefault();
-    if (persons.every(nameCheck=>nameCheck.name!==newName)){
-    setPersons([...persons, { name: newName }]);
-    setNewName("");
-    }else{
-      setNewName('');
-      return alert(`${newName} is already added to phonebook`)
+    if (persons.every((nameCheck) => nameCheck.name !== newName)) {
+      setPersons([...persons, { name: newName, number: newNumber }]);
+      setNewName("");
+      setNewNumber("");
+    } else {
+      setNewName("");
+      setNewNumber("");
+      return alert(`${newName} is already added to phonebook`);
     }
-
   };
 
-  const onChangeForm = (event) => {
+  const onChangeName = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
+  };
+
+  const onChangeNumber = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+
+  const onFilter = (event) => {
+    console.log(event.target.value);
+    //ensure special character does not break the filter
+    const regex = /[^\-a-zA-Z0-9' ]+/gi;
+    let filteredInput = event.target.value.replace(regex, "");
+    setFilter(filteredInput);
+  };
+
+  const filterTest = ({ name }) => {
+    console.log(name);
+    const filteringNames = new RegExp(filter, "ig");
+    return filteringNames.test(name);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={filter} onChange={onFilter} />
+      </div>
       <form onSubmit={submitName}>
         <div>
-          name: <input value={newName} onChange={onChangeForm} />
+          <h2>add a new</h2>
+          name: <input value={newName} onChange={onChangeName} /> <br />
+          number: <input value={newNumber} onChange={onChangeNumber} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -34,8 +67,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
-          <div key={person.name}>{person.name}</div>
+        {persons.filter(filterTest).map((person) => (
+          <div key={person.name}>
+            {person.name} {person.number}
+          </div>
         ))}
       </div>
     </div>
