bf2e914b6e8832ceeb5fcb008bf02e13ae9d6d2a
Jemyishai <jesse.moskowitz@gmail.com>
2020-12-18 12:23:39 -0500

    everything from 4C & beginning of 4D

--- part4/blog/controller/blogs.js
+++ part4/blog/controller/blogs.js
@@ -1,14 +1,14 @@
 const blogsRouter = require("express").Router();
-const { response } = require("../app");
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response, next) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 } );
   response.json(blogs);
 });
 
 blogsRouter.get("/:id", async (req, res, next) => {
-  const foundBlog = await Blog.findById(req.params.id);
+  const foundBlog = await Blog.findById(req.params.id).populate('user', { username: 1, name: 1 });
   if (foundBlog) {
     res.json(foundBlog);
   } else {
@@ -23,25 +23,54 @@ blogsRouter.post("/", async (request, response, next) => {
     body.likes = 0;
   }
 
-  if (!body.url || !body.title) {
-    response.status(400).end();
+  if (!body.userId) {
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+    });
+    if (!body.url || !body.title) {
+      response.status(400).end();
+    } else {
+      const savedBlog = await blog.save();
+      response.status(201).json(savedBlog);
+    }
   } else {
-    const blog = new Blog(body);
-    const savedBlog = await blog.save();
-    response.status(201).json(savedBlog);
+    const user = await User.findById(body.userId);
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id,
+    });
+
+    if (!body.url || !body.title) {
+      response.status(400).end();
+    } else {
+      const savedBlog = await blog.save();
+      user.blogs = user.blogs.concat(savedBlog._id);
+      await user.save();
+      response.status(201).json(savedBlog);
+    }
   }
 });
 
-
-blogsRouter.put("/:id", async (request, response, next) =>{
-  const {likes, title, author, url} = request.body;
-  await Blog.findByIdAndUpdate(request.params.id, { likes, title, author, url}, {
-    new: true,
-    runValidators: true,
-    context: 'query'
-  })
-  response.status(200).end()
-})
+blogsRouter.patch("/:id", async (request, response, next) => {
+  const { likes, title, author, url } = request.body;
+  await Blog.findByIdAndUpdate(
+    request.params.id,
+    { likes, title, author, url },
+    {
+      new: true,
+      runValidators: true,
+      context: "query",
+    }
+  );
+  response.status(200).end();
+});
 
 blogsRouter.delete("/:id", async (request, response, next) => {
   await Blog.findByIdAndRemove(request.params.id);
---
+++ part4/blog/controller/users.js
@@ -0,0 +1,48 @@
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+    id: 1
+  });
+  response.json(users);
+});
+
+usersRouter.get("/:id", async (request, response) => {
+  const userFound = await User.findById(request.params.id).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+    id: 1
+  });
+  if (userFound) {
+    response.json(userFound);
+  } else {
+    response.status(404).end();
+  }
+});
+
+usersRouter.post("/", async (request, response) => {
+  const body = request.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.json(savedUser);
+});
+
+module.exports = usersRouter;
