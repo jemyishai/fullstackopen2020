5a3b76b940793adcc1eda9f5784b0a729c5d5a5d
Jemyishai <jesse.moskowitz@gmail.com>
2020-12-23 09:00:33 -0500

    added token handling to post requests

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get("/", async (request, response, next) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 } );
@@ -16,29 +17,26 @@ blogsRouter.get("/:id", async (req, res, next) => {
   }
 });
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post("/", async (request, response, next) => {
   const body = request.body;
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!body.likes) {
     body.likes = 0;
   }
-
-  if (!body.userId) {
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes,
-    });
-    if (!body.url || !body.title) {
-      response.status(400).end();
-    } else {
-      const savedBlog = await blog.save();
-      response.status(201).json(savedBlog);
-    }
-  } else {
-    const user = await User.findById(body.userId);
-
     const blog = new Blog({
       title: body.title,
       author: body.author,
@@ -55,7 +53,6 @@ blogsRouter.post("/", async (request, response, next) => {
       await user.save();
       response.status(201).json(savedBlog);
     }
-  }
 });
 
 blogsRouter.patch("/:id", async (request, response, next) => {
