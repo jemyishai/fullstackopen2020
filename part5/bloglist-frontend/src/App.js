1d1ba05cfb7973dcd87fce859037bc63ce24a23d
Jemyishai <jesse.moskowitz@gmail.com>
2021-01-08 08:08:28 -0500

    Make the login permanent by using the local storage

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -2,18 +2,31 @@ import React, { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
-import Notification from './Notification.js';
+import Notification from "./Notification.js";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
-  const [ errorMessage, setErrorMessage ] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  }, []);
+
+  const reset = () => {
+    setUsername("");
+    setPassword("");
+  };
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -23,58 +36,59 @@ const App = () => {
         username,
         password,
       });
+      window.localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
       setUser(user);
-      setUsername("");
-      setPassword("");
+      reset()
     } catch (exception) {
-      setErrorMessage('Wrong credentials')
+      setErrorMessage("Wrong credentials");
       setTimeout(() => {
-        setErrorMessage(null)
+        setErrorMessage(null);
       }, 5000);
-      setUsername('')
-      setPassword('')
+      reset()
     }
   };
 
-  if (user === null) {
-    return (
-      <div>
-      <Notification notification={errorMessage} />
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-            <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-            <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    );
-  }
+  const userLogin = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
 
-  return (
+  const blogDisplays = () => (
     <div>
       <h2>blogs</h2>
-
       <p> {user.name} logged in</p>
-
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
+    </div>
+  );
+
+  return (
+    <div>
+      <Notification notification={errorMessage} />
+      {user === null ? userLogin() : blogDisplays()}
     </div>
   );
 };
