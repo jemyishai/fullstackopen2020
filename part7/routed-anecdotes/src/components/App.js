f87edcf3429a53ff5d4ec6613a1fb50fbab31cf8
Jemyishai <jesse.moskowitz@gmail.com>
2021-06-14 11:20:35 -0400

    7a ex 7.1 initial routes

--- part7/routed-anecdotes/src/components/App.js
+++ part7/routed-anecdotes/src/components/App.js
@@ -1,60 +1,91 @@
-import React, { useState } from 'react'
-import Menu from './Menu';
-import Footer from './Footer';
-import About from './About';
-import AnecdoteList from './AnecdoteList';
-import CreateNew from './CreateNew';
-
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Footer from "./Footer";
+import About from "./About";
+import AnecdoteList from "./AnecdoteList";
+import CreateNew from "./CreateNew";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
     {
-      content: 'If it hurts, do it more often',
-      author: 'Jez Humble',
-      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      content: "If it hurts, do it more often",
+      author: "Jez Humble",
+      info: "https://martinfowler.com/bliki/FrequencyReducesDifficulty.html",
       votes: 0,
-      id: '1'
+      id: "1",
     },
     {
-      content: 'Premature optimization is the root of all evil',
-      author: 'Donald Knuth',
-      info: 'http://wiki.c2.com/?PrematureOptimization',
+      content: "Premature optimization is the root of all evil",
+      author: "Donald Knuth",
+      info: "http://wiki.c2.com/?PrematureOptimization",
       votes: 0,
-      id: '2'
-    }
-  ])
+      id: "2",
+    },
+  ]);
 
-  const [notification, setNotification] = useState('')
+  const [notification, setNotification] = useState("");
 
   const addNew = (anecdote) => {
-    anecdote.id = (Math.random() * 10000).toFixed(0)
-    setAnecdotes(anecdotes.concat(anecdote))
-  }
+    anecdote.id = (Math.random() * 10000).toFixed(0);
+    setAnecdotes(anecdotes.concat(anecdote));
+  };
 
-  const anecdoteById = (id) =>
-    anecdotes.find(a => a.id === id)
+  const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
 
   const vote = (id) => {
-    const anecdote = anecdoteById(id)
+    const anecdote = anecdoteById(id);
 
     const voted = {
       ...anecdote,
-      votes: anecdote.votes + 1
-    }
+      votes: anecdote.votes + 1,
+    };
 
-    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
-  }
+    setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
+  };
+
+  const padding = {
+    paddingRight: 5,
+  };
 
   return (
     <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
+    <h1>Software anecdotes</h1>
+
+    <Router>
+
+        <Link to="/" style={padding}>
+          anecdotes
+        </Link>
+        <Link to="create" style={padding}>
+          create new
+        </Link>
+        <Link to="about" style={padding}>
+          about
+        </Link>
+
+      <Switch>
+        <Route path="/create">
+          <CreateNew addNew={addNew} />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <AnecdoteList anecdotes={anecdotes} />
+        </Route>
+      </Switch>
+
+    </Router>
+    <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- part7/routed-anecdotes/src/components/Menu.js
+++ part7/routed-anecdotes/src/components/Menu.js
@@ -1,21 +1,25 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
 
 const Menu = () => {
   const padding = {
     paddingRight: 5,
   };
   return (
-    <div>
-      <a href="#" style={padding}>
-        anecdotes
-      </a>
-      <a href="#" style={padding}>
-        create new
-      </a>
-      <a href="#" style={padding}>
-        about
-      </a>
-    </div>
+      <Router>
+        <Link to="/" style={padding}>
+          anecdotes
+        </Link>
+        <Link to="create" style={padding}>
+          create new
+        </Link>
+        <Link to="about" style={padding}>
+          about
+        </Link>
+      </Router>
   );
 };
 
